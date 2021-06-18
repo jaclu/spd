0018c638c5743b0211bf7f7c8693f03d970c71c5
jaclu <jacob.lundqvist@gmail.com>
2021-06-18 15:56:28 +0200

    warns when no task params are set

--- scripts/m_tasks_apk.sh
+++ scripts/m_tasks_apk.sh
@@ -137,6 +137,8 @@ _run_this() {
     # Perform the task / tasks independently, convenient for testing
     # and debugging.
     #
+    [ -z "$SPD_APKS_DEL" ] && [ -z "$SPD_APKS_DEL" ] && \
+        warning_msg "None of the task variables set"
     task_mta_update
     [ -n "$SPD_APKS_DEL" ] && task_mta_remove_unwanted
     task_mta_upgrade
