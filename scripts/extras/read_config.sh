c3aaa16399f2b323198fdb4f844552186c9c07ba
jaclu <jacob.lundqvist@gmail.com>
2021-06-19 01:23:16 +0200

    va

--- scripts/extras/read_config.sh
+++ scripts/extras/read_config.sh
@@ -103,10 +103,10 @@ _read_cfg_file() {
     verbose_msg "will read: $cfg_file"
     . "$cfg_file"
     
-    msg_3 "After reading $cfg_file"
-    echo "Current state of SPD_APKS_DEL:"
-    echo "$SPD_APKS_DEL"
-    echo
+    #msg_3 "After reading $cfg_file"
+    #echo "Current state of SPD_APKS_DEL:"
+    #echo "$SPD_APKS_DEL"
+    #echo
     unset cfg_file
 }
 
