b96a80984f147659474909990b92f486f52a596b
jaclu <jacob.lundqvist@gmail.com>
2023-02-01 13:14:20 +0100

    cleaned up obsolete functionality

--- scripts/m_tasks_etc_files.sh
+++ scripts/m_tasks_etc_files.sh
@@ -18,11 +18,8 @@
 #=====================================================================
 
 # shellcheck disable=SC2034
-script_tasks="task_replace_some_etc_files  - replaces hosts and repositories if so requested
-task_patch_etc_files         - Fixes some files with issues"
+script_tasks="task_replace_some_etc_files  - replaces hosts and repositories if so requested"
 script_description="Will fix /etc/inittab, and if so requested replace some /etc files"
-
-
 
 #=====================================================================
 #
@@ -33,16 +30,16 @@ script_description="Will fix /etc/inittab, and if so requested replace some /etc
 
 help_local_parameters() {
     echo "SPD_FILE_HOSTS$(
-        test -z "$SPD_FILE_HOSTS" \
-        && echo '        - will replace /etc/hots' \
-        || echo "=$SPD_FILE_HOSTS" )"
+        test -z "$SPD_FILE_HOSTS" &&
+            echo '        - will replace /etc/hots' ||
+            echo "=$SPD_FILE_HOSTS"
+    )"
     echo "SPD_FILE_REPOSITORIES$(
-        test -z "$SPD_FILE_REPOSITORIES" \
-        && echo ' - will replace /etc/apk/repositories' \
-        || echo "=$SPD_FILE_REPOSITORIES" )"
+        test -z "$SPD_FILE_REPOSITORIES" &&
+            echo ' - will replace /etc/apk/repositories' ||
+            echo "=$SPD_FILE_REPOSITORIES"
+    )"
 }
-
-
 
 #=====================================================================
 #
@@ -64,18 +61,10 @@ task_replace_some_etc_files() {
     # If the config file is not found, no action will be taken
     check_abort
 
-    [ -n "$SPD_FILE_HOSTS" ] &&  _tef_copy_etc_file "$SPD_FILE_HOSTS" /etc/hosts
+    [ -n "$SPD_FILE_HOSTS" ] && _tef_copy_etc_file "$SPD_FILE_HOSTS" /etc/hosts
+    [ -n "$SPD_FILE_REPOSITORIES" ] && _tef_copy_etc_file "$SPD_FILE_HOSTS" /etc/apt/repositories
     echo
 }
-
-task_patch_etc_files() {
-    msg_2 "Patching some /etc files"
-    check_abort
-    # _tef_fix_inittab
-    echo
-}
-
-
 
 #=====================================================================
 #
@@ -93,7 +82,6 @@ _tef_expand_all_deploy_paths() {
     SPD_FILE_REPOSITORIES=$(expand_deploy_path "$SPD_FILE_REPOSITORIES")
 }
 
-
 _tef_copy_etc_file() {
 
     tcef_src="$1"
@@ -102,21 +90,21 @@ _tef_copy_etc_file() {
 
     if [ -n "$tcef_extra" ]; then
         error_msg "_tef_copy_etc_file($tcef_src, $tcef_dst) [$tcef_extra]" \
-                  "more than 2 params given!"
+            "more than 2 params given!"
     fi
     verbose_msg "_tef_copy_etc_file($tcef_src, $tcef_dst)"
     if [ -n "$tcef_src" ]; then
         msg_3 "$tcef_dst"
         if [ ! -f "$tcef_src" ]; then
             error_msg "_tef_copy_etc_file() src_file[$tcef_src] " \
-                      "NOT FOUND!\n$tcef_src"
+                "NOT FOUND!\n$tcef_src"
         fi
         if [ "$SPD_TASK_DISPLAY" != "1" ]; then
             rm "$tcef_dst" # to avoid incomplete removal of prev vers of file
             cp "$tcef_src" "$tcef_dst"
             echo "$tcef_src"
-        elif [ "$SPD_TASK_DISPLAY" = "1" ] \
-                && [ "$SPD_DISPLAY_NON_TASKS" = "1" ]; then
+        elif [ "$SPD_TASK_DISPLAY" = "1" ] &&
+            [ "$SPD_DISPLAY_NON_TASKS" = "1" ]; then
             echo "Will NOT be modified"
         fi
     fi
@@ -124,46 +112,6 @@ _tef_copy_etc_file() {
     unset tcef_src
     unset tcef_extra
 }
-
-
-_tef_not_fix_inittab() {
-    tfi_inittab_file="/etc/inittab"
-    verbose_msg "_tef_cleanup_inittab()"
-     msg_3 "Inspecting $tfi_inittab_file"
-
-    # Since iSH has no concept of consoles getty lines are pointless
-    if grep -q 'getty' "$tfi_inittab_file"; then
-        msg_3 "Removing gettys"
-        if [ "$SPD_TASK_DISPLAY" != "1" ]; then
-           sed -i '/getty/d' "$tfi_inittab_file"
-            echo "done!"
-        else
-            echo "will happen"
-       fi
-    fi
-
-    # Get rid of mostly non functional openrc config lines
-    tfi_ok_line="::sysinit:/sbin/openrc default"
-    if grep openrc "$tfi_inittab_file" | grep -q -v "$tfi_ok_line"; then
-        msg_3 "Fixing openrc related content"
-        if ! grep -q "$tfi_ok_line" /etc/inittab ; then
-            if [ "$SPD_TASK_DISPLAY" != "1" ]; then
-                sed -i '/::sysinit/d' "$tfi_inittab_file"
-                sed -i '/::wait:/d' "$tfi_inittab_file"
-                echo "$tfi_ok_line"  >> "$tfi_inittab_file"
-                echo "done!"
-            else
-                echo "will happen"
-            fi
-        else
-            echo "Patch already applied!"
-        fi
-    fi
-
-    unset tfi_inittab_file
-    unset tfi_ok_line
-}
-
 
 #=====================================================================
 #
