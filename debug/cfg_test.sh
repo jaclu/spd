a6617dd45d1af4ecd055fd78a837f21fda70b330
jaclu <jacob.lundqvist@gmail.com>
2026-04-10 23:04:11 +0200

    using expand_config_var

--- debug/cfg_test.sh
+++ debug/cfg_test.sh
@@ -9,41 +9,14 @@ d_here="$(dirname "$(dirname "$(realpath "$0")")")"
 
 read_config_file "$d_here"/debug/dummy_config.yml
 
-# expand_var2() {
-#     _ev_varname=$1
-#     _ev_needle='${'
-#     while eval "_ev_val=\"\$$_ev_varname\""; do
-#         # shellcheck disable=SC2154
-#         case $_ev_val in
-#             *"$_ev_needle"*) ;;
-#             *) break ;;
-#         esac
-#         eval "$_ev_varname=\"$_ev_val\""
-#     done
-# }
-
-expand_var() {
-    _ev_varname=$1
-    while eval "_ev_val=\"\$$_ev_varname\""; do
-        # shellcheck disable=SC2154
-        [ "$_ev_val" = "${_ev_val#*\$\{}" ] && break
-        eval "$_ev_varname=\"$_ev_val\""
-    done
-}
-
 echo "Verify variables retrieved"
 # shellcheck disable=SC2154
 {
-    # expand_var SPD_UNAME
-    expand_var SPD_HOME_DIR_CONTENT
+    expand_config_var SPD_HOME_DIR_CONTENT
+    expand_config_var SPD_UNAME
 
     eval foo=\$SPD_HOME_DIR_CONTENT
     echo "SPD_UNAME:            [$SPD_UNAME]"
     echo "SPD_HOME_DIR_CONTENT: [$SPD_HOME_DIR_CONTENT]"
-    # echo "foo [$foo]"
-    # echo "$SPD_HOME_DIR_CONTENT"
-    # echo
-    # SPD_HOME_DIR_CONTENT="custom/tars/home_${SPD_UNAME}.tgz"
-    # echo "$SPD_HOME_DIR_CONTENT"
 
 }
