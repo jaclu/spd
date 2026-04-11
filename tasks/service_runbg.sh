6bfdb7f3c21df1d3f142a4a705d75e87000230ca
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2026-04-11 13:23:09 +0200

    some cleanup

--- tasks/service_runbg.sh
+++ tasks/service_runbg.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 deploy_initd_script() {
-    cp "$_svc_init_scr_src" "$svc_script" || {
-        err_msg "$module_name: Failed to copy $_svc_init_scr_src"
+    cp "$_svc_init_scr_org" "$svc_script" || {
+        err_msg "$module_name: Failed to copy $_svc_init_scr_org"
     }
 }
 
@@ -26,41 +26,38 @@ task_prepare() {
     # setting up any environmental dependencies in order for task_execute to be executed,
     # such as installing dependencies if need be etc
     # is_linux || err_msg "Will not run apt on non-Linux"
-
-    check_for_abort 1 task_prepare
-
     dependency_issue=0
     svc_script=/etc/init.d/runbg
+
+    check_for_abort 1 task_prepare
 
     expand_config_var SPD_SERVICE_HANDLER
     if [ -n "$SPD_SERVICE_HANDLER" ]; then
         echo "SPD_SERVICE_HANDLER: $SPD_SERVICE_HANDLER"
         [ "$SPD_SERVICE_HANDLER" = openrc ] && {
+            # only used with openrc
             command -v openrc >/dev/null 2>&1 || {
                 lbl_2 "$module_name: Dependency issue - openrc not found"
                 dependency_issue=1
             }
+            expand_config_var SPD_SVC_RUNBG_RUNLVL
+            if [ -n "$SPD_SVC_RUNBG_RUNLVL" ]; then
+                echo "SPD_SVC_RUNBG_RUNLVL: $SPD_SVC_RUNBG_RUNLVL"
+            else
+                lbl_2 "$module_name: Dependency issue - SPD_SVC_RUNBG_RUNLVL not defined"
+                dependency_issue=1
+            fi
         }
 
-        _svc_init_scr_src="$DEPLOY_PATH/files/services/$SPD_SERVICE_HANDLER/runbg"
-        [ -f "$_svc_init_scr_src" ] || {
-            lbl_2 "$module_name: Service script not found: $_svc_init_scr_src"
+        _svc_init_scr_org="$DEPLOY_PATH/files/services/$SPD_SERVICE_HANDLER/runbg"
+        [ -f "$_svc_init_scr_org" ] || {
+            lbl_2 "$module_name: Service script not found: $_svc_init_scr_org"
             dependency_issue=1
         }
     else
         lbl_2 "$module_name: Dependency issue - SPD_SERVICE_HANDLER not defined"
         dependency_issue=1
     fi
-
-    expand_config_var SPD_SVC_RUNBG_RUNLVL
-    [ "$SPD_SERVICE_HANDLER" = "openrc" ] && {
-        if [ -n "$SPD_SVC_RUNBG_RUNLVL" ]; then
-            echo "SPD_SVC_RUNBG_RUNLVL: $SPD_SVC_RUNBG_RUNLVL"
-        else
-            lbl_2 "$module_name: Dependency issue - SPD_SVC_RUNBG_RUNLVL not defined"
-            dependency_issue=1
-        fi
-    }
 
     [ -d /etc/init.d ] || {
         lbl_2 "$module_name: Dependency issue - /etc/init.d not found"
@@ -72,15 +69,14 @@ task_prepare() {
 task_execute() {
     check_for_abort 0 task_execute
 
-    # deploy_initd_script
-    # [ -f "$svc_script" ] || err_msg "Service script not found: $svc_script"
+    deploy_initd_script
 
-    # # perform the actual task
-    # case "$SPD_SERVICE_HANDLER" in
-    #     'openrc') handler_openrc ;;
-    #     'sysv-init') handler_sysv ;;
-    #     *) err_msg "$module_name: Unrecognized service-handler: $SPD_SERVICE_HANDLER" ;;
-    # esac
+    # attach service to handler
+    case "$SPD_SERVICE_HANDLER" in
+        'openrc') handler_openrc ;;
+        'sysv-init') handler_sysv ;;
+        *) err_msg "$module_name: Unrecognized service-handler: $SPD_SERVICE_HANDLER" ;;
+    esac
 }
 
 #=====================================================================
@@ -97,5 +93,4 @@ task_execute() {
 }
 module_name="service_runbg.sh"
 
-task_prepare || return 1
-task_execute
+task_prepare && task_execute
