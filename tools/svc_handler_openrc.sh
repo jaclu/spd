8568593a3ff0d11e1e251df070246a5183debab2
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2026-04-11 13:04:58 +0200

    impoved error mgs fo svc_handler_openrc.sh

--- tools/svc_handler_openrc.sh
+++ tools/svc_handler_openrc.sh
@@ -8,17 +8,23 @@ svc_runlevel_set() {
     for rl in $(rc-status -l); do
         [ "$rl" = "$_srs_srvc_runlevel" ] && continue
         if rc-status "$rl" | grep -qw "$_srs_srvc_name"; then
-            rc-update del "$_srs_srvc_name" "$rl" || exit 30
+            rc-update del "$_srs_srvc_name" "$rl" || {
+                m="svc_handler_openrc.sh: Failed cmd -"
+                m="$m rc-update del $_srs_srvc_name $rl"
+                err_msg "$m"
+            }
             echo "$_srs_srvc_name removed from runlevel: $rl"
         fi
     done
 
     # Ensure in desired runlevel
-    if ! rc-update show "$_srs_srvc_runlevel" | grep -q "$(basename "$_srs_srvc_name")"; then
+    if ! rc-update show "$_srs_srvc_runlevel" \
+        | grep -q "$(basename "$_srs_srvc_name")"; then
+
         rc-update add "$_srs_srvc_name" "$_srs_srvc_runlevel" || {
-            _m="svc_handler_openrc.sh: Failed to add $_srs_srvc_name"
-            _m="$_m to runlevel $_srs_srvc_runlevel"
-            err_msg "$_m"
+            m="svc_handler_openrc.sh: Failed cmd -"
+            m="$m rc-update add $_srs_srvc_name $_srs_srvc_runlevel"
+            err_msg "$m"
         }
         echo "$_srs_srvc_name added to runlevel: $_srs_srvc_runlevel"
     fi
