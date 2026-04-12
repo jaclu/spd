96ecb8bf37fb807ae57846a6b1df139a1077742d
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2026-04-13 01:25:10 +0200

    added more default configs parsed

--- tools/process_config_hierarchy.sh
+++ tools/process_config_hierarchy.sh
@@ -21,22 +21,20 @@ read_config_file "$DEPLOY_PATH"/configs/defaults.yml
 # platform remated
 is_linux && read_config_file "$DEPLOY_PATH"/configs/platform/linux.yml
 is_macos && read_config_file "$DEPLOY_PATH"/configs/platform/macos.yml
-# echo "SPD_SVC_RUNBG_RUNLVL: $SPD_SVC_RUNBG_RUNLVL"
 is_chrooted_ish && read_config_file "$DEPLOY_PATH"/configs/platform/ish.yml
 
-# # Package handler
-# command -v apk >/dev/null && read_config_file "$DEPLOY_PATH"/configs/file_systems/alpine.yml
-# command -v brew >/dev/null && read_config_file "$DEPLOY_PATH"/configs/file_systems/pkg_homebrew.yml
+# file system related
+fs_is_alpine && read_config_file "$DEPLOY_PATH"/configs/file_systems/alpine.yml
+fs_is_debian && read_config_file "$DEPLOY_PATH"/configs/file_systems/debian.yml
+fs_is_devuan && read_config_file "$DEPLOY_PATH"/configs/file_systems/devuan.yml
+fs_is_ubuntu && read_config_file "$DEPLOY_PATH"/configs/file_systems/ubuntu.yml
 
 # service related
-# echo "post ish: SPD_SVC_RUNBG_RUNLVL: $SPD_SVC_RUNBG_RUNLVL"
 read_config_file "$DEPLOY_PATH"/configs/services/autossh.yml
 read_config_file "$DEPLOY_PATH"/configs/services/runbg.yml
 
 # hostname specific
 read_config_file "$DEPLOY_PATH/configs/hostname/$(hostname -s | tr '[:upper:]' '[:lower:]').yml"
 
-msg_dbg "SPD_ABORT: $SPD_ABORT" 1
-
-# shellcheck disable=SC2034
+# shellcheck disable=SC2034 # SPD_CONFIG_PROCESSED used by caller to determine if this has been run
 SPD_CONFIG_PROCESSED="Done by: $0"
