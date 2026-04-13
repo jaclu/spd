cac60a635ed257c3443ddd084034317cf3333807
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2026-04-13 02:49:12 +0200

    ensure service name is provided

--- tools/svc_handler_common.sh
+++ tools/svc_handler_common.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 check_service_env() {
+    _cse_svc="$1"
+    [ -z "$_cse_svc" ] && {
+        # shellcheck disable=SC2154 # module_name defined by caller
+        err_msg "$module_name: check_service_env() called with empty service name"
+    }
 
     if [ -n "$SPD_SERVICE_HANDLER" ]; then
         # shellcheck disable=SC2154 # D_REPO defined by $0
-        _svc_init_scr_org="$D_REPO/files/services/$SPD_SERVICE_HANDLER/autossh"
+        _svc_init_scr_org="$D_REPO/files/services/$SPD_SERVICE_HANDLER/$_cse_svc"
         [ -f "$_svc_init_scr_org" ] || {
             lbl_2 "${module_name:-}: Service script not found: $_svc_init_scr_org"
             dependency_issue=1
