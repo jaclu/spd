df1bd22f0a59c7ab0d9b47ab086c0b5e6470d2d6
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2026-04-22 16:29:21 +0200

    code reshuffle

--- tools/prepare-env.sh
+++ tools/prepare-env.sh
@@ -112,6 +112,53 @@ cleanup_custom() {
 
 #---------------------------------------------------------------------
 #
+#   Option parsing
+#
+#---------------------------------------------------------------------
+
+indicate_unset() {
+    case "$1" in
+        '') echo "*unset*" ;;
+        *) echo "$1" ;;
+    esac
+}
+
+cmd_line_param_list() {
+    _lbl="${1:-Listing of cmd line options}"
+    lbl_2 "$_lbl"
+    lbl_4 "  opt_task    $(indicate_unset "$opt_task")"
+}
+
+cmd_line_param_parse() {
+    opt_task=install # defaults to install
+    while [ -n "$1" ]; do
+        case "$1" in
+            install) opt_task=install ;; # handling tasks sending it as a param
+            remove) opt_task=remove ;;   # Remove service etc, not accepted by all tasks
+            force | force-install)
+                # Used to force an install in cases where the task displays a warning
+                # and aborts on install, example a service not properly working on
+                # the used platform
+                opt_task=force-install
+                ;;
+            *)
+                cmd_line_param_list
+                err_msg "Unrecognized major option: $1"
+                ;;
+        esac
+        shift
+    done
+    cmd_line_param_list
+}
+
+cmd_line_param_error() {
+    lbl_1 "Invalid command-line param"
+    # cmd_line_param_list "Processed options"
+    err_msg "$1"
+}
+
+#---------------------------------------------------------------------
+#
 #   Dependency handling
 #
 # SPD_ABORT is a global typically set per host config that limits what tasks can be done
@@ -227,53 +274,6 @@ alpine_release_ge() {
     # numeric compare
     [ "$curM" -gt "$reqM" ] \
         || { [ "$curM" -eq "$reqM" ] && [ "$curm" -ge "$reqm" ]; }
-}
-
-#---------------------------------------------------------------------
-#
-#   Option parsing
-#
-#---------------------------------------------------------------------
-
-indicate_unset() {
-    case "$1" in
-        '') echo "*unset*" ;;
-        *) echo "$1" ;;
-    esac
-}
-
-cmd_line_param_list() {
-    _lbl="${1:-Listing of cmd line options}"
-    lbl_2 "$_lbl"
-    lbl_4 "  opt_task    $(indicate_unset "$opt_task")"
-}
-
-cmd_line_param_parse() {
-    opt_task=install # defaults to install
-    while [ -n "$1" ]; do
-        case "$1" in
-            install) opt_task=install ;; # handling tasks sending it as a param
-            remove) opt_task=remove ;;   # Remove service etc, not accepted by all tasks
-            force | force-install)
-                # Used to force an install in cases where the task displays a warning
-                # and aborts on install, example a service not properly working on
-                # the used platform
-                opt_task=force-install
-                ;;
-            *)
-                cmd_line_param_list
-                err_msg "Unrecognized major option: $1"
-                ;;
-        esac
-        shift
-    done
-    cmd_line_param_list
-}
-
-cmd_line_param_error() {
-    lbl_1 "Invalid command-line param"
-    # cmd_line_param_list "Processed options"
-    err_msg "$1"
 }
 
 #---------------------------------------------------------------------
