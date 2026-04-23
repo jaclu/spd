31b0f9681dfd16bd147151e167056a88924f17fb
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2026-04-23 13:06:02 +0200

    work on debug procedure

--- debug/cfg-test.sh
+++ debug/cfg-test.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
 
+test_variable_retrieval() {
+    expand_config_var SPD_UNAME
+
+    echo "Verify variables retrieved"
+    # shellcheck disable=SC2154 # SPD_ vars read via config files
+    {
+        # expand references to other variables recursively
+        expand_config_var SPD_HOME_DIR_CONTENT
+        expand_config_var SPD_UNAME
+        
+        eval foo=\$SPD_HOME_DIR_CONTENT
+        echo "SPD_UNAME:            [$SPD_UNAME]"
+        echo "SPD_HOME_DIR_CONTENT: [$SPD_HOME_DIR_CONTENT]"
+        echo "SPD_NO_BLANKS:        [$SPD_NO_BLANKS]"
+        echo "SPD_WITH_BLANKS:      [$SPD_WITH_BLANKS]"
+        echo "SPD_APK_MAN:          [$SPD_APK_MAN]"
+    }
+}
+
+test_variable_defaults() {
+    
+    expand_config_var SPD_EMPTY_NOT jupp
+    expand_config_var SPD_EMPTY  yes
+
+    echo "SPD_EMPTY_NOT [$SPD_EMPTY_NOT]"
+    echo "SPD_EMPTY [$SPD_EMPTY]"
+
+}
+
+
 #=====================================================================
 #
 #   Main
@@ -18,20 +48,9 @@ D_REPO=$(cd -- "$(dirname -- "$0")/.." && pwd)
 
 . "$D_REPO"/tools/process-yaml-config_file.sh
 
+
 parse_yaml_config_file "$D_REPO"/configs/task_overrides/filesystem_alpine.yml
 parse_yaml_config_file "$D_REPO"/debug/dummy_config.yml
 
-echo "Verify variables retrieved"
-# shellcheck disable=SC2154 # SPD_ vars read via config files
-{
-    # expand references to other variables recursively
-    expand_config_var SPD_HOME_DIR_CONTENT
-    expand_config_var SPD_UNAME
-
-    eval foo=\$SPD_HOME_DIR_CONTENT
-    echo "SPD_UNAME:            [$SPD_UNAME]"
-    echo "SPD_HOME_DIR_CONTENT: [$SPD_HOME_DIR_CONTENT]"
-    echo "SPD_NO_BLANKS:        [$SPD_NO_BLANKS]"
-    echo "SPD_WITH_BLANKS:      [$SPD_WITH_BLANKS]"
-    echo "SPD_APK_MAN:          [$SPD_APK_MAN]"
-}
+# test_variable_defaults
+test_variable_retrieval
